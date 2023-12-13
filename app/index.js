ed2ba296feb8f5a3d5ceb5a69278ca6f8785b9b1
Kadin Vo <kadinvo@gmail.com>
2023-12-13 13:59:24 +1300

    Added layout and index

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+// View can be seen as Div and Text as general P tag
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  )
+}
